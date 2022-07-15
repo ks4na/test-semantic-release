5c4dcdec61d656980330106f5a9ea978cfa11d61
qiuwei <qiuwei@fullgoalos.com.cn>
2022-07-15 17:57:50 +0800

    chore(semantic-release): 配置不需要在 npm 发包

--- release.config.js
+++ release.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    // '@semantic-release/npm', // 不需要在 npm 发布包
+    '@semantic-release/github',
+  ],
 }
